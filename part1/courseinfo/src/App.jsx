b872aecec2fbb1fa57f908f299643b2a1e24c372
daka <daniel.karlgren@captor.se>
2024-10-21 10:10:26 +0200

    new code

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable react/prop-types */
+
+const Header = (props) => {
+
+    return (
+      <h1>{props.course}</h1>
+    )
+
+}
+const Part = (props) => {
+
+  return (<p>{props.part.name} {props.part.exercises}</p>)
+}
+
+const Content = (props) => {
+  const part1 =props.parts[0]
+  const part2 =props.parts[1]
+  const part3 =props.parts[2]
+  return (
+  <div>
+    <Part part={part1} />
+    <Part part={part2} />
+    <Part part={part3} />
+  </div>
+  )
+}
+
+const Total = (props) => {
+  return (<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>)
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
