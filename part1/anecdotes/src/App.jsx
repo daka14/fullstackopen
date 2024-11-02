ae63e527bb3ea439fe679af3a7185b3a982d5531
daka14 <daniel.karlgren@captor.se>
2024-11-02 10:15:22 +0100

    finnished part1/annecdote

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 
+const AnecdoteDisplay = (props) => (<div>
+  <h1>Anecdote of the day</h1>
+  {props.anecdotes[props.selected]}
+  
+  <p>has {props.votes[props.selected]} votes</p>  
+  
+  </div>)
 
+const AnecdoteStatistics = (props) => {
+  if (props.mostVoted < 0) 
+    return (<div>
+      <h1>Anecdote with most votes</h1>
+      <p>No votes casted yet</p>
+      </div>)
+  else 
+    return (<div>
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[props.mostVoted]}</p>
+      </div>)
+  
+}
 
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
   </button>
 )
-const votes = new Array(8).fill(0)
+
+
+
+
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -19,29 +43,49 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
+  const votes_init = new Array(anecdotes.length).fill(0) 
+
   const [selected, setSelected] = useState(0)
-  const [votes, setVote] = useState([0,0,0,0,0,0,0,0])
-  const handleClick = () => {
+  const [mostVoted, setMostVoted] = useState(-1)
+  const [votes, setVote] = useState(votes_init)
+
+  
+
+  const findMostVoted = (array) => {
    
-      let newVotes =[...votes]
-     
-      let vote = newVotes[selected]
-      newVotes[selected] = vote +1
+    let maxIndex = 0
+
+    for (let i =1; i<array.length;i++){
+      if (array[maxIndex] < array[i]) maxIndex = i
+    }
+    
+    return maxIndex
+  }
+
+  const handleVote = () => {
+   
+    const newVotes =[...votes]
+    const vote = newVotes[selected]
+
+    newVotes[selected] = vote +1
       
     setVote(newVotes)
-
+    
+    const newMostVoted = findMostVoted(newVotes)
+    
+    setMostVoted(newMostVoted)
+    
   }
+
+   
 
   return (
     <div>
-      {anecdotes[selected]}
-      <div>
-      <p>has {votes[selected]} votes</p>  
-      <Button handleClick={handleClick} text="vote" />
-      <Button handleClick={() => setSelected(Math.floor(Math.random() * 7))} text="next anecdote" />
-      
-      </div>
+     <AnecdoteDisplay anecdotes={anecdotes} votes={votes} selected={selected} />
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={() => setSelected(Math.floor(Math.random() * (anecdotes.length -1)))} text="next anecdote" />
+      <AnecdoteStatistics anecdotes={anecdotes} mostVoted={mostVoted} />     
     </div>
   )
 }
