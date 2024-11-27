0ab8d0d7a11dcf1acde2cbb6e73ab144fde075d8
daka <daniel.karlgren@captor.se>
2024-11-27 17:02:53 +0100

    moving on

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,15 +1,42 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+
 import Note from './components/Note'
 import noteService from './services/notes'
 
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
+const Footer = () => {  
+  const footerStyle = {    
+  color: 'green',    
+  fontStyle: 'italic',    
+  fontSize: 16  
+
+}  
+return (    
+<div style={footerStyle}>      
+<br />      
+<em>Note app, Department of Computer Science, University of Helsinki 2024</em>    
+</div>  
+
+)}
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {    
     noteService
@@ -43,20 +70,29 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
   const toggleImportanceOf = id => {
-    
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-  
+
     noteService
-    .update(id, changedNote)
-    .then(returnedNote => {
-      setNotes(notes.map(note => note.id === id ? returnedNote : note))
-    })
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <ul>
         {notes.map(note => 
           <Note 
@@ -73,7 +109,8 @@ const App = (props) => {
           onChange={handleNoteChange}
         />
         <button type="submit">save</button>
-      </form>   
+      </form>  
+      <Footer /> 
     </div>
   )
 }
