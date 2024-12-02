ffb258f1f0d43ba956450806457b012c44572015
daka14 <daniel.karlgren@captor.se>
2024-12-02 05:44:05 +0100

    countries

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,9 +1,56 @@
 import { useState, useEffect } from 'react'
 import countriesService from './services/countries'
 
+const Country = ({country}) => {
+  console.log(country)
+  return (<div>
+    <h1>{country.name.common}</h1>
+    <p>
+      capital {country.capital}
+    </p>
+    <p>
+      area {country.area}
+    </p>
+    <h3>
+      languages:
+    </h3>
+    <ul>
+      {Object.values(country.languages).map((value,index) => 
+        <li key={index} >
+           {value}
+           </li>
+       )} 
+        
+    </ul>
+    <div>
+      {country.flag}
+    </div>
+    </div>)
+  
+}
+
+const AllCountries = ({countries, filter}) =>{
+  
+  const filteredCountries = countries.filter(country => 
+    country.name.common
+    .toLowerCase().includes(filter.toLowerCase()))
+  
+  if (filteredCountries.length ===1){
+  return(<Country country={filteredCountries[0]} />)
+    }
+  else if(filteredCountries.length <= 10){
+    return (filteredCountries.map(country => 
+      <div key={country.ccn3}> <ShortCountry country={country} /></div>))
+  }
+}
+
+const ShortCountry =({country}) =>{
+  return (<div>{country.name.common}</div>)
+}
+
 const Filter = (props) => {
   return (<div>
-    filter: <input 
+    find country: <input 
     value={props.newFilter}
     onChange={props.handleNewFilter}/>
   </div>)
@@ -27,6 +74,7 @@ const App = () => {
   }  
   
   if (countries){
+    
     return (
       <div>
         <h1>Countries</h1>
@@ -34,7 +82,9 @@ const App = () => {
         newFilter={newFilter} 
         handleNewFilter={handleNewFilter} 
       />
-        <div>{countries[0].name.common}</div>
+      
+      <AllCountries countries={countries} filter={newFilter} />
+        
         
         </div>
     )
